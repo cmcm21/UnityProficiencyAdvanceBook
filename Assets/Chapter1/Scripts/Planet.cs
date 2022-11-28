1e72ed0c21bd81194d7221ea941ce417b3335c94
Miguelon <miguel.correa@tagwizz.com.mx>
2022-11-27 22:11:53 -0600

    planet orbit added

--- Assets/Chapter1/Scripts/Planet.cs
+++ Assets/Chapter1/Scripts/Planet.cs
@@ -1,22 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    private float _rotationalSpeed = 10f;
-    private float _orbitalSpeed = 0.20f;
-    private float _orbitalAngle = 0.0f;
-    private float _angle = 0.0f;
-    private float _orbitalRotatinalSpeed = 20;
-    private float _distanceToSun = 150;
+    [SerializeField] private float rotationalSpeed = 10f;
+    [SerializeField] private float orbitalSpeed = 0.20f;
+    [SerializeField] private float orbitalAngle = 0.0f;
+    [SerializeField] private float angle = 0.0f;
+    [SerializeField] private float orbitalRotationalSpeed = 20;
+    [SerializeField] private float distanceToSun = 150;
+    [SerializeField] private Color orbitColor = Color.blue;
+    [SerializeField] private int lenghtOfOrbitRenderer = 100;
     
     private GameObject _sun;
     
     private void Start()
     {
        _sun = GameObject.FindWithTag("Sun");
-       transform.position = new Vector3(_distanceToSun, 0, _distanceToSun);
+       transform.position = new Vector3(distanceToSun, 0, distanceToSun);
+       DrawOrbit();
     }
 
     private void Update()
@@ -25,12 +29,38 @@ public class Planet : MonoBehaviour
        MovePlanet();
     }
 
+    private void DrawOrbit()
+    {
+       LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+       lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Additive"));
+
+       lineRenderer.startColor = orbitColor;
+       lineRenderer.endColor = orbitColor;
+       lineRenderer.startWidth = 1.0f;
+       lineRenderer.endWidth = 1.0f;
+       lineRenderer.positionCount = lenghtOfOrbitRenderer + 1;
+
+       float unitAngle = (2 * Mathf.PI) / lenghtOfOrbitRenderer;// angle of each vertex to made a circle
+       for (int i = 0; i <= lenghtOfOrbitRenderer; i++)
+       {
+          float currentAngle = unitAngle * i;
+          
+          Vector3 vertex = new Vector3(
+             distanceToSun * Mathf.Cos(currentAngle), // cos to form a circle
+             0,
+             distanceToSun * (float)Math.Sin(currentAngle) // sin to form a circle
+          );
+          
+          lineRenderer.SetPosition(i, vertex);
+       }
+    }
+
 
     private void RotatePlanet()
     {
        transform.Rotate(
           Vector3.up,
-          _rotationalSpeed * Time.deltaTime,
+          rotationalSpeed * Time.deltaTime,
           Space.World
           );
     }
@@ -39,10 +69,10 @@ public class Planet : MonoBehaviour
     {
        Vector3 newPosition;
        
-       _orbitalAngle += Time.deltaTime * _orbitalSpeed;
+       orbitalAngle += Time.deltaTime * orbitalSpeed;
 
-       newPosition.x = _sun.transform.position.x + _distanceToSun * Mathf.Cos(_orbitalAngle);
-       newPosition.z = _sun.transform.position.z + _distanceToSun * Mathf.Sin(_orbitalAngle);
+       newPosition.x = _sun.transform.position.x + distanceToSun * Mathf.Cos(orbitalAngle);
+       newPosition.z = _sun.transform.position.z + distanceToSun * Mathf.Sin(orbitalAngle);
        newPosition.y = _sun.transform.position.y;
 
        transform.position = newPosition;
